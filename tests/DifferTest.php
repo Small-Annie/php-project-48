cf12df5d9eb2cfd14b7cf45604b3dee497fb9673
Petrova Anna <petr230999@gmail.com>
2025-07-29 14:34:38 +0300

    Move all test logic to DifferTest.php
    Refactor tests by relocating logic from FormattersTest.php and BaseTestCase.php to DifferTest.php for improving test organization.

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -2,29 +2,56 @@
 
 namespace Differ\Differ\Tests;
 
-use Differ\Tests\BaseTestCase;
+use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
 
-use function Differ\Differ\buildDiff;
-use function Differ\Parsers\parse;
+use function Differ\Differ\genDiff;
 
-class DifferTest extends BaseTestCase
+class DifferTest extends TestCase
 {
-    #[DataProvider('buildDiffProvider')]
-    public function testBuildDiff(string $expectedDiffFile, string $firstFile, string $secondFile): void
+    private function getFixtureFullPath(string $fixtureName): string
     {
-        $expectedDiff = include $this->getFixtureFullPath($expectedDiffFile);
-        $file1 = parse($this->getFixtureFullPath($firstFile));
-        $file2 = parse($this->getFixtureFullPath($secondFile));
+        $parts = [__DIR__, 'fixtures', $fixtureName];
+        $fullPath = realpath(implode(DIRECTORY_SEPARATOR, $parts));
 
-        $this->assertEquals($expectedDiff, buildDiff($file1, $file2));
+        if ($fullPath === false) {
+            throw new \RuntimeException("File not found: " . implode('/', $parts));
+        }
+
+        return $fullPath;
     }
 
-    public static function buildDiffProvider(): array
+    private function getFileContents(string $fixtureName): string
+    {
+        $content = file_get_contents($this->getFixtureFullPath($fixtureName));
+        if ($content === false) {
+            throw new \RuntimeException("Failed to read the file: " . $fixtureName);
+        }
+        return $content;
+    }
+
+    #[DataProvider('genDiffProvider')]
+    public function testGenDiff(string $expectedFormattedFile, string $firstFile, string $secondFile, ?string $format): void
+    {
+        $expectedFormattedContent = $this->getFileContents($expectedFormattedFile);
+
+        $path1 = $this->getFixtureFullPath($firstFile);
+        $path2 = $this->getFixtureFullPath($secondFile);
+
+        $actualFormattedContent = $format === null
+            ? genDiff($path1, $path2)
+            : genDiff($path1, $path2, $format);
+
+        $this->assertEquals($expectedFormattedContent, $actualFormattedContent);
+    }
+
+    public static function genDiffProvider(): array
     {
         return [
-            "json files" => ['diff.php', 'file1.json', 'file2.json'],
-            "yaml files" => ['diff.php', 'file1.yaml', 'file2.yaml'],
+            "default (stylish)" => ['diffFormat.stylish', 'file1.json', 'file2.yaml', null],
+            "stylish" => ['diffFormat.stylish', 'file1.json', 'file2.yaml', 'stylish'],
+            "plain" => ['diffFormat.plain', 'file1.json', 'file2.yaml', 'plain'],
+            "json" => ['diffFormat.json', 'file1.json', 'file2.yaml', 'json']
         ];
     }
 }
